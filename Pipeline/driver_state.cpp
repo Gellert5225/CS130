aa8e1ffceff75a307d40e3d68b8e723b054703da
gellert5225 <gellert.li1995@gmail.com>
2020-02-19 22:55:16 -0800

    pipeline started

--- Pipeline/driver_state.cpp
+++ Pipeline/driver_state.cpp
@@ -40,13 +40,19 @@ void render(driver_state& state, render_type type)
         case render_type::triangle: {
             const data_geometry* in[3];
             for (int j = 0; j < 3; j++) {
-                data_geometry* d_g = new data_geometry();
+                data_geometry *d_g = new data_geometry();
+                data_vertex d_v;
+                d_v.data = new float[MAX_FLOATS_PER_VERTEX];
                 d_g->data = new float[MAX_FLOATS_PER_VERTEX];
                 for (int i = 0; i < state.floats_per_vertex; i++) {
                     d_g->data[i] = state.vertex_data[i + j * 3];
-                }   
+                    d_v.data[i] = state.vertex_data[i + j * 3];
+                }
+                
+                state.vertex_shader(d_v, *d_g, state.uniform_data);
                 in[j] = d_g;
-            }       
+            } 
+            
             rasterize_triangle(state, in);  
             break;
         }
@@ -81,12 +87,8 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
         dv.data = new float[MAX_FLOATS_PER_VERTEX];
         for (int i = 0; i < MAX_FLOATS_PER_VERTEX; i++) {
             dv.data[i] = in[j]->data[i];
-            printf("%f\n", in[j]->data[i]);
         }
-
-        state.vertex_shader(dv, dg, state.uniform_data);
-        points[j] = dg.gl_Position;
-        printf("gl_position: %f\n", dg.gl_Position[0]);
+        points[j] = vec4(dv.data[0], dv.data[1], 0.0, 0.0);
     }
 
     int width = state.image_width;
@@ -94,17 +96,32 @@ void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 
     // from NDC to pixel space
     float a_x = 0.5 * (1 + points[0][0]) * width;
-    float a_y = 0.5 * (1 - points[0][1]) * height;
+    float a_y = 0.5 * (1 + points[0][1]) * height;
     float b_x = 0.5 * (1 + points[1][0]) * width;
-    float b_y = 0.5 * (1 - points[1][1]) * height;
+    float b_y = 0.5 * (1 + points[1][1]) * height;
     float c_x = 0.5 * (1 + points[2][0]) * width;
-    float c_y = 0.5 * (1 - points[2][1]) * height;
+    float c_y = 0.5 * (1 + points[2][1]) * height;
 
     float p_x, p_y;
-    float i, j;
     float alpha, beta, gamma;
+    vec2 v0 = {b_x - a_x, b_y - a_y};
+    vec2 v1 = {c_x - a_x, c_y - a_y};
+    vec2 v2;
+
+    float ABC = dot(v0, v0) * dot(v1, v1) - dot(v0, v1) * dot(v0, v1);
 
     for (int p = 0; p < width * height; p++) {
-        state.image_color[p] = make_pixel(255.0, 0.0, 0.0);
+        // map index to pixel coord
+        p_x = p % width;
+        p_y = p / width;
+
+        // calculate barycentric coord
+        v2 = {p_x - a_x, p_y - a_y};
+        beta = (dot(v1, v1) * dot(v2, v0) - dot(v0, v1) * dot(v2, v1)) / ABC;
+        gamma = (dot(v0, v0) * dot(v2, v1) - dot(v0, v1) * dot(v2, v0)) / ABC;
+        alpha = 1.0 - beta - gamma;
+
+        if (beta >= 0 && gamma >= 0 && alpha >= 0) 
+            state.image_color[p] = make_pixel(255, 255, 255);
     }
 }
--- Pipeline/grading/driver_state.cpp
+++ Pipeline/grading/driver_state.cpp
@@ -18,7 +18,7 @@ void initialize_render(driver_state& state, int width, int height)
 {
     state.image_width = width;
     state.image_height = height;
-    state.image_color = new pixel();
+    state.image_color = 0;
     state.image_depth = 0;
     
     state.image_color = new pixel[width * height];
@@ -40,20 +40,23 @@ void render(driver_state& state, render_type type)
         case render_type::triangle: {
             const data_geometry* in[3];
             for (int j = 0; j < 3; j++) {
-                data_geometry* d_g = new data_geometry();
+                data_geometry *d_g = new data_geometry();
+                data_vertex d_v;
                 d_g->data = new float[MAX_FLOATS_PER_VERTEX];
                 for (int i = 0; i < state.floats_per_vertex; i++) {
-                    d_g[0].data[i] = state.vertex_data[i];
+                    d_g->data[i] = state.vertex_data[i + j * 3];
                 }   
                 in[j] = d_g;
-            }       
+                
+                state.vertex_shader(d_v, *d_g, state.uniform_data);
+            } 
+            
             rasterize_triangle(state, in);  
             break;
         }
         default:
             break;
     }
-
 }
 // This function clips a triangle (defined by the three vertices in the "in" array).
 // It will be called recursively, once for each clipping face (face=0, 1, ..., 5) to
@@ -75,16 +78,50 @@ void clip_triangle(driver_state& state, const data_geometry* in[3], int face)
 // fragments, calling the fragment shader, and z-buffering.
 void rasterize_triangle(driver_state& state, const data_geometry* in[3])
 {
-    data_vertex dv;
-    dv.data = new float[MAX_FLOATS_PER_VERTEX];
+    vec4 points[3];
     for (int j = 0; j < 3; j++) {
+        data_geometry dg;
+        data_vertex dv; 
+        dv.data = new float[MAX_FLOATS_PER_VERTEX];
         for (int i = 0; i < MAX_FLOATS_PER_VERTEX; i++) {
             dv.data[i] = in[j]->data[i];
         }
+
+        //state.vertex_shader(dv, dg, state.uniform_data);
+        points[j] = vec4(dv.data[0], dv.data[1], 0.0, 0.0);
     }
 
-    data_geometry dg;
-    
-    state.vertex_shader(dv, dg, state.uniform_data);
-}
+    int width = state.image_width;
+    int height = state.image_height;
 
+    // from NDC to pixel space
+    float a_x = 0.5 * (1 + points[0][0]) * width;
+    float a_y = 0.5 * (1 + points[0][1]) * height;
+    float b_x = 0.5 * (1 + points[1][0]) * width;
+    float b_y = 0.5 * (1 + points[1][1]) * height;
+    float c_x = 0.5 * (1 + points[2][0]) * width;
+    float c_y = 0.5 * (1 + points[2][1]) * height;
+
+    float p_x, p_y;
+    float alpha, beta, gamma;
+    vec2 v0 = {b_x - a_x, b_y - a_y};
+    vec2 v1 = {c_x - a_x, c_y - a_y};
+    vec2 v2;
+
+    float ABC = dot(v0, v0) * dot(v1, v1) - dot(v0, v1) * dot(v0, v1);
+
+    for (int p = 0; p < width * height; p++) {
+        // map index to pixel coord
+        p_x = p % width;
+        p_y = p / width;
+
+        // calculate barycentric coord
+        v2 = {p_x - a_x, p_y - a_y};
+        beta = (dot(v1, v1) * dot(v2, v0) - dot(v0, v1) * dot(v2, v1)) / ABC;
+        gamma = (dot(v0, v0) * dot(v2, v1) - dot(v0, v1) * dot(v2, v0)) / ABC;
+        alpha = 1.0 - beta - gamma;
+
+        if (beta >= 0 && gamma >= 0 && alpha >= 0) 
+            state.image_color[p] = make_pixel(255, 255, 255);
+    }
+}
