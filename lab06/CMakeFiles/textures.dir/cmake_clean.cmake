5717f2bb8624c7cbb9c512cb1c7c7023fd877caa
Gellert Li <gellertli@gellerts-mbp.home>
2020-03-03 17:55:06 -0800

    texture lab

---
+++ lab06/CMakeFiles/textures.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/textures.dir/application.cpp.o"
+  "CMakeFiles/textures.dir/gl_arcball.cpp.o"
+  "CMakeFiles/textures.dir/gl_viewer.cpp.o"
+  "CMakeFiles/textures.dir/main.cpp.o"
+  "CMakeFiles/textures.dir/obj.cpp.o"
+  "textures"
+  "textures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/textures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
